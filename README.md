b38484d2f0bdb3dfed5f8d1273ee534587aab485
Roland Shum <roland.shum@digipen.edu>
2021-05-25 21:08:26 -0700

    Initial commit. CMakes seems to be working. Embedded stbi, tinyobjloader, glfw, and glm to the project.
    CMakes finds vulkan.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # GlassEye-Renderer
+
 My own implementation of a Vulkan renderer.
+
+# Requirements
+
+> = CMake 3.17 C++ 20 (project uses MSVC with CLion)
+
+# Dependancies
+
+Uses C++ 20. MSVC currently supports the most C++ 20 features out of all compilers
+
+## All of these libraries are embedded into the project for stability
+
+- ENTT
+- GLM
+- STBI (texture loading)
+- Tiny Obj Loader (obj loading)
+- GLFW
+- Luna SDK (Vulkan)
+- ImGui
---
+++ Source/src/stdafx.h
@@ -0,0 +1,33 @@
+#define GLFW_INCLUDE_VULKAN
+
+#include <GLFW/glfw3.h>
+
+#include <algorithm>  // Necessary for std::min/std::max
+#include <array>
+#include <cstdint>  // Necessary for UINT32_MAX
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <set>
+#include <stdexcept>
+#include <unordered_map>
+#include <vector>
+#include <chrono>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include "stb_image.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+
+#include <tiny_obj_loader.h>
