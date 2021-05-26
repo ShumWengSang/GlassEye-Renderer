7f9aa2340b7337fbc2bfef1fa36e055b79b3aa7b
Roland Shum <roland.shum@digipen.edu>
2021-05-25 22:40:31 -0700

    Modified shader compile file such that it can run in both linux as a sh and in windows as a bat.

--- Assets/Shaders/compile.bat
+++ Assets/Shaders/compile.bat
@@ -1,3 +1,19 @@
+
+echo off
+
+goto(){
+glslc shader.vert -o vert.spv
+glslc shader.frag -o frag.spv
+read -p "Press enter to continue"
+}
+
+goto $@
+exit
+
+:(){
+rem Windows script here
 glslc.exe shader.vert -o vert.spv
 glslc.exe shader.frag -o frag.spv
 pause
+echo %OS%
+exit
